59d6cd49415a67f9ec461ee0bbbee6874d8aa5c3
Siwakorn Anunaua <siwakornanunaua@gmail.com>
2018-10-19 18:50:04 +0700

    ทำยังไม่เสร็จ

--- LabSqLite/app/src/main/java/th/ac/kku/cs/labsqlite/Student.java
+++ LabSqLite/app/src/main/java/th/ac/kku/cs/labsqlite/Student.java
@@ -1,4 +1,27 @@
 package th.ac.kku.cs.labsqlite;
 
 public class Student {
+    public static final String TABLE_NAME = "students";
+    public static final String COLUMN_ID = "std_id";
+    public static final String COLUMN_NAME = "std_name";
+
+    private String id;
+    private String name;
+// Create table SQL query
+    public static final String CREATE_TABLE =
+        "CREATE TABLE " + TABLE_NAME + "("
+                + COLUMN_ID + " TEXT,"
+                + COLUMN_NAME + " TEXT"
+                +")";
+
+    public Student (String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+public Student () {}
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
 }
